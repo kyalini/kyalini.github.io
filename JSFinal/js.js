58bf84cea8704c717356a649944566a132f1e623
kyalini <kyli3612@colorado.edu>
2024-05-03 22:38:36 -0600

    js update

--- JSFinal/js.js
+++ JSFinal/js.js
@@ -31,20 +31,20 @@ window.onload = function() {
 }
 
 function setGame() {
-    for (let i = 0; i < 9; i++) { //i goes from 0 to 8, stops at 9
+    for (let i = 0; i < 16; i++) { //i goes from 0 to 8, stops at 9
         //<div id="0-8"></div>
         let tile = document.createElement("div");
         tile.id = i.toString();
         tile.addEventListener("click", selectTile);
         document.getElementById("board").appendChild(tile);
     }
-    setInterval(setPom, 2000); // 1000 miliseconds = 1 second, every 1 second call setMole
+    setInterval(setPom, 1000); // 1000 miliseconds = 1 second, every 1 second call setMole
     setInterval(setPlant, 2000); // 2000 miliseconds = 2 seconds, every 2 second call setPlant
 }
 
 function getRandomTile() {
-    //math.random --> 0-1 --> (0-1) * 9 = (0-9) --> round down to (0-8) integers
-    let num = Math.floor(Math.random() * 9);
+    //math.random --> 0-1 --> (0-1) * 16 = (0-16) --> round down to (0-8) integers
+    let num = Math.floor(Math.random() * 16);
     return num.toString();
 }
 
